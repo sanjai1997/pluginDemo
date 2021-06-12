813c602895b8b7260fc134af6b5da7c670752bbe
Sanjai Suresh <s.sanjai007@gmail.com>
2021-06-13 01:17:21 +0530

    intiate Commit

---
+++ GitPluginDemo1Prefix/GitPluginDemo1Prefix/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPluginDemo1Prefix
+{
+    public class Class1
+    {
+    }
+}
