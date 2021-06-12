8ec2116fb3a8656fa713217ef08b9dfb0ac92383
Sanjai Suresh <s.sanjai007@gmail.com>
2021-06-13 01:23:29 +0530

    ImplmentedTheIplugin

--- GitPluginDemo1Prefix/GitPluginDemo1Prefix/Class1.cs
+++ GitPluginDemo1Prefix/GitPluginDemo1Prefix/Class1.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
 
 namespace GitPluginDemo1Prefix
 {
-    public class Class1
+    public class Class1 : IPlugin
     {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
